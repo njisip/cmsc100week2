20838fd8ae84e5170f85d4b052c0580ec6578c95
laubsofficial <laubreyual@gmail.com>
2015-08-19 11:32:13 +0800

    CMSC 100 Week 2

---
+++ controllers/teacher.js
@@ -0,0 +1,7 @@
+exports.find = function(req, res) {
+	res.send('Get teacher');
+};
+
+exports.add = function(req, res) {
+	res.send('Add teacher');
+};
---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.use(require('body-parser')());
+app.use(require('method-override')());
+app.use(require(__dirname + '/config/router')(express.Router()));
+
+// app.get('/', function(req, res){
+// 	res.send('Hello World!');
+// });
+
+// app.get('/students', function(req, res){
+// 	res.send('NJ');
+// });
+
+var server = app.listen(5000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('Example app listening at htpp://%s:%s', host, port);
+});
