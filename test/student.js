435bd42e88aa4fdc07a7de386e2c932cda04fb37
njisip <cyber2103@gmail.com>
2015-09-02 12:40:34 +0800

    Testing student.js

---
+++ test/student.js
@@ -0,0 +1,52 @@
+var should = require('should-http'),
+	request = require('supertest');
+
+describe('Student', function() {
+	var url =  "http://localhost:5000";
+	var studrec = {
+		'name': 'Nards John',
+		'studno': '2014-42641',
+	}
+
+	describe('find()', function() {
+		it('Should retrieve all student record', function(done) {
+			request(url)
+			.get('/students')
+			.end(function(err, res) {
+				if(err) throw err;
+				res.should.have.status(200);
+				res.body.should.be.an.instanceOf(Array);
+				done();
+			});
+		});
+
+	});
+
+	describe('findOne()', function() {
+		it('Should retrieve a specific student record', function(done) {
+			request(url)
+			.get('/students/2')
+			.end(function(err, res) {
+				if(err) throw err;
+				res.should.have.status(200);
+				done();
+			});
+		});
+
+	});
+
+	describe('insert()', function() {
+		it('Insert a new student record', function(done) {
+			request(url)
+			.post('/students')
+			.send(studrec)
+			.end(function(err, res) {
+				if(err) throw err;
+				res.should.have.status(200);
+				done();
+			});
+		});
+
+	});
+
+});
